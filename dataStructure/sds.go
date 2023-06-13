3e910c3cc8cd736995bfc57642df9fa111daa5ff
maximus <1091261998@qq.com>
2023-06-13 18:07:32 +0800

    sds2

--- dataStructure/sds.go
+++ dataStructure/sds.go
@@ -1,14 +1,59 @@
 package dataStructure
 
+import (
+	"fmt"
+	"unsafe"
+)
+
 //用于指向sdshdr的buf属性
 type sds *rune
 
 type sdshdr struct {
-	len  int
+	//长度
+	len int
+	//预留空间
 	free int
-	buf  []rune
+	//字符数组
+	buf []rune
 }
 
-func sdsnew(s string) sdshdr {
+//创建一个包含字符串s的sds
+func sdsNew(s string) sds {
+	initLen := len(s)
+	return sdsNewLen(s, initLen)
+}
 
+//创建一个空的sds
+func sdsEmpty() sds {
+	return sdsNewLen("", 0)
+}
+
+func sdsNewLen(s string, initLen int) sds {
+	sh := &sdshdr{initLen, 0, []rune(s)}
+	//将数组sh.buf的地址赋给指针sds
+	fmt.Println(&sh)
+	fmt.Println(&sh.buf[0])
+	fmt.Println(&sh.buf[0] - &sh)
+	return &sh.buf[0]
+}
+
+//func sdsFree(s sds) {}
+
+//返回已使用的长度
+func sdsLen(s sds) int {
+	var sds1 sdshdr
+	t := unsafe.Sizeof(sds1)
+	var sds2 sdshdr
+	t2 := unsafe.Sizeof(sds2)
+	fmt.Println(t)
+	fmt.Println(t2)
+	unSafeS := unsafe.Pointer(s)
+	unSafeSh := uintptr(unSafeS) - t
+	sh := (*sdshdr)(unsafe.Pointer(unSafeSh))
+	return sh.len
+}
+
+func Test() {
+	sd := sdsNew("abc")
+	fmt.Println(sdsLen(sd))
 }
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import "godis/dataStructure"
 
 func main() {
-	fmt.Println("hello world")
-	//s := "abc你好"
-	var b []rune = []rune{'你', 's'}
-	for _, v := range b {
-		fmt.Println(v)
-	}
-	fmt.Println(len(b))
+	//fmt.Println("hello world")
+	////s := "abc你好"
+	//var b []rune = []rune{'你', 's'}
+	//for _, v := range b {
+	//	fmt.Println(v)
+	//}
+	//fmt.Println(len(b))
+	dataStructure.Test()
+	//a := []int{10, 100, 200}
+	//var ptr [3]*int
+	//var p *[]int
+	//p = &a
+	//fmt.Println(p)
+	//for i := 0; i < 3; i++ {
+	//	ptr[i] = &a[i]
+	//	fmt.Printf("a[%d]的地址：%x\n", i, ptr[i])
+	//
+	//}
+	//for i := 0; i < 3; i++ {
+	//	fmt.Printf("a[%d]的值是：%d\n", i, *ptr[i])
+	//}
+
 }
